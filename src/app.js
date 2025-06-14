4b2210b48021285f2d6107ae7df6478e3bc5184c
DeepakFlokis <nitmzdeepakyadav@gmail.com>
2025-06-14 23:46:03 +0530

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const connectDB = require("./config/database");
+const app = express();
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const http = require("http");
+
+require("dotenv").config();
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(cookieParser());
+
+const authRouter = require("./routes/auth");
+
+app.use("/", authRouter);
+
+const server = http.createServer(app);
+
+connectDB()
+  .then(() => {
+    console.log("Database connection established...");
+    server.listen(process.env.PORT, () => {
+      console.log("Server is successfully listening on port 7777...");
+    });
+  })
+  .catch((err) => {
+    console.error("Database cannot be connected!!");
+  });
